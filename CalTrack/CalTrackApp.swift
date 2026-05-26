392728a6bfd82257d3cc55b7353e449869bf5d5a
Jose <josemlopez@bpd.com.do>
2026-05-26 19:34:31 -0400

    Initial Commit

---
+++ CalTrack/CalTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalTrackApp.swift
+//  CalTrack
+//
+//  Created by Jose Miguel Lopez on 26/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CalTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalTrackTests/CalTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalTrackTests.swift
+//  CalTrackTests
+//
+//  Created by Jose Miguel Lopez on 26/5/26.
+//
+
+import Testing
+@testable import CalTrack
+
+struct CalTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
